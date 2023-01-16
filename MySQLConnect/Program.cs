02ca2db4072321572f29dd8083ffdd558ca8a152
Юрий Иванов <jura401@mail.ru>
2023-01-16 21:05:13 +0300

    Добавьте файлы проекта.

---
+++ MySQLConnect/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Net.Configuration;
+
+namespace MySQLConnect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection mysql = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySQL"].ConnectionString);
+            mysql.Open();
+            Console.WriteLine("Open");
+            mysql.CreateCommand();
+            MySqlCommand mySqlCommand = new MySqlCommand("select count(*) FROM Books", mysql);
+            MySqlDataReader reader = mySqlCommand.ExecuteReader();
+            if (reader.Read())
+                Console.WriteLine(reader.GetValue(0));
+            mysql.Close();
+            Console.WriteLine("Close");
+
+            Console.ReadLine();
+        }
+    }
+}
